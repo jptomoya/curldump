23783c64523022bb29fb29815672e3d74833966a
jptomoya <4786564+jptomoya@users.noreply.github.com>
2023-04-23 16:49:44 +0900

    Check if the uid exists to avoid adduser error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,13 @@
 set -eu
 
 uid=$(stat -c '%u' .)
-adduser -D -u "$uid" user
-adduser user wireshark
+su_user="user"
 
-exec su user -c "/curldump.sh $*"
+if ! cut -d: -f3 /etc/passwd | grep -x -q "$uid"; then
+  adduser -D -u "$uid" $su_user
+else
+  su_user=$(awk -F: "\$3==$uid {print \$1}" /etc/passwd)
+fi
+adduser "$su_user" wireshark
+
+exec su -s /bin/sh "$su_user" -c "/curldump.sh $*"
